d98569cc1ad3288d80ce0af24904fd4c98ab9a4e
hbw <1446039171@qq.com>
2022-10-31 09:43:48 +0800

    template

--- plopfile.js
+++ plopfile.js
@@ -43,12 +43,17 @@ module.exports = (plop) => {
       {
         type: 'add',
         path: 'stories/{{properCase name}}/index.vue',
-        templateFile: 'template/stories/componentDocs/index.hbs'
+        templateFile: 'template/stories/index.hbs'
       },
       {
         type: 'add',
-        path: 'stories/{{kebabCase name}}/index.stories.js',
-        templateFile: 'template/stories/.vuepress/components/index.hbs'
+        path: 'stories/{{properCase name}}/index.stories.js',
+        templateFile: 'template/stories/index.stories.hbs'
+      },
+      {
+        type: 'add',
+        path: 'stories/{{properCase name}}/index.scss',
+        templateFile: 'template/stories/scss.hbs'
       },
     ]
   })
