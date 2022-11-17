2015d39966c32ae321edd6dd3d7ec6211b620991
黄博文 <1446039171@qq.com>
2022-11-17 14:12:14 +0800

    'deploy'

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 const path = require('path')
 module.exports = {
+  core: {
+    builder: "webpack5"
+  },
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-storysource'
+    '@storybook/addon-storysource',
   ],
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
@@ -13,7 +16,6 @@ module.exports = {
       use: ['style-loader', 'css-loader', 'sass-loader'],
       include: path.resolve(__dirname, '../')
     })
-
     // Return the altered config
     return config
   },
--- Mark.md
+++ Mark.md
@@ -9,6 +9,13 @@
    <!-- 2. 安装 async-validator: "^1.11.5", -->
    <!-- "async-validator": "^4.1.1", -->
 
+### 兼容故事书跟程序 
+故事书默认webpack4 而vue-cli用的5.0对应的webpack需要5以上的版本，所以以前要切换版本运行。
+更改故事书默认的webpack版本，即俩套程序均可使用5以上的webpack。
+（webpack5 有些版本也不支持文档运行,此处用的是"webpack": "5.21.2"才将程序运行起来）
+参考此文章： https://blog.csdn.net/vv_bug/article/details/113845376
+
+
 <!-- Node.js v14.19.1. -->
 
 ### 库打包 npm run build
