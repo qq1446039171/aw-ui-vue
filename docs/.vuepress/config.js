625b57d48ff72f4ac9f3b76124d72e6b8954dcee
黄博文 <1446039171@qq.com>
2023-02-21 09:16:47 +0800

    build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
       }
     ] // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: './', // 这是部署到github相关的配置
+  // base: './', // 这是部署到github相关的配置
   markdown: {
     lineNumbers: false // 代码块显示行号
   },
